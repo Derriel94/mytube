01b9da1601c6fe95300fd06fbc5db322ddf4df9d
Derriel Collins <derrielcollins96@gmail.com>
2024-03-09 19:38:51 -0600

    created final stage of components started channelDetail page

--- src/components/ChannelDetail.tsx
+++ src/components/ChannelDetail.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React,{ useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom'
+import { Box } from '@mui/material';
+import { fetchFromApi } from './../utils/fetchFromApi'
 
 function ChannelDetail() {
+
+	const { channelid } = useParams();
+
+
+
+	useEffect(()=> {
+	
+
+	},[channelid])
+
 	return (
-		<div>
-			ChannelDetail
-		</div>
+		<Box sx={{height: "95vh"}}>
+			
+		</Box>
 	)
 }
 
