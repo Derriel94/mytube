791957c4c7bdbee67432f889e3cec1398a9a9fa0
Derriel Collins <derrielcollins96@gmail.com>
2024-03-10 07:39:26 -0500

    testing api call to receive channel details,returning nothing so far

--- src/components/ChannelDetail.tsx
+++ src/components/ChannelDetail.tsx
@@ -1,22 +1,32 @@
 import React,{ useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom'
-import { Box } from '@mui/material';
+import { Box, Paper, Stack } from '@mui/material';
 import { fetchFromApi } from './../utils/fetchFromApi'
 
 function ChannelDetail() {
 
 	const { channelid } = useParams();
+	const [channelDetail, setChannelDetail] = useState();
 
 
 
 	useEffect(()=> {
-	
-
+		fetchFromApi(`channels?part=snippet,statistics&id=${channelid}`)
+		.then((data) => {
+			console.log(data)
+		})
 	},[channelid])
 
 	return (
 		<Box sx={{height: "95vh"}}>
-			
+			<Stack sx={{display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
+				<Paper elevation={3} sx={{background: "blue"}}>
+
+				</Paper>
+				<Box>
+
+				</Box>
+			</Stack>
 		</Box>
 	)
 }
